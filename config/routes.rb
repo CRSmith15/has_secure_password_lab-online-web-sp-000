c697c7a511e8131cf8e904dc47462acb242e0ee5
Chance Smith <chancesmith97@hotmail.com>
2020-08-29 22:20:21 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
 
-  get '/login' => 'sessions#new'
-  post '/login' => 'sessions#create'
-  get '/welcome' => 'sessions#show'
-  post '/logout' => 'sessions#destroy'
+  get '/', to: 'users#new', as: 'user'
+  post '/', to: 'users#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/:user', to: 'sessions#show', as: 'homepage'
+  post '/logout', to: 'sessions#destroy',
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
